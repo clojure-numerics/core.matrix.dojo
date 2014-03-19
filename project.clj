936a5aafbbcde4b3445b2a09c0b88e116b11d23b
mikera <mike.r.anderson.13@gmail.com>
2014-03-19 14:20:48 +0800

    add project.clj

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject core.matrix.dojo "0.0.1-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 
+                 [org.clojure/java.jdbc "0.3.3"]
+                 [net.mikera/clojure-utils "0.6.0"]
+                 [net.mikera/core.matrix "0.20.0"]
+              ]
+
+  :source-paths ["src/main/clojure"]
+  :test-paths ["src/test/clojure"]
+  
+  :resource-paths ["src/main/resources"]
+)
