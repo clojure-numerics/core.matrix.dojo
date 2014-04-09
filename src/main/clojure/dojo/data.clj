e9cfd5a3644e4bbabdb0c9ca382248f611f1318b
mikera <mike.r.anderson.13@gmail.com>
2014-04-09 16:15:16 +0800

    add EPL team result data loading

--- src/main/clojure/dojo/data.clj
+++ src/main/clojure/dojo/data.clj
@@ -1,2 +1,24 @@
-(ns dojo.data)
+(ns dojo.data
+  (:use [clojure.core.matrix])
+  (:require [clojure.data.csv :as csv])
+  (:require [clojure.java.io :as io]))
 
+;; load all data from the CVS file in src/main/resources
+(def DATA-LINES 
+  (with-open [in-file (io/reader (io/resource "EPL-2012-13.csv"))]
+    (doall (csv/read-csv in-file))))
+
+;; The headings from the CVS file
+(def HEADINGS (first DATA-LINES))
+
+;; The table of results
+(def RESULTS (vec (rest DATA-LINES)))
+
+;; Map of column names to column numbers
+(def COL-NUMS (zipmap HEADINGS (range)))
+
+;; Collect the list  of all teams
+(def TEAMS (vec (sort (into #{} (slice RESULTS 1 (COL-NUMS "HomeTeam"))))))
+
+;; check we have the right number of total teams - there should be 20!
+(count TEAMS)
