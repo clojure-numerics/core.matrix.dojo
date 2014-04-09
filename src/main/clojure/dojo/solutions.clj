df4035e02913b1cd8a49a822c06d8ee40a77b8c4
mikera <mike.r.anderson.13@gmail.com>
2014-04-09 22:18:11 +0800

    checks

--- src/main/clojure/dojo/solutions.clj
+++ src/main/clojure/dojo/solutions.clj
@@ -23,6 +23,10 @@
 (def DRAWS (mapv #(if (= "D" (mget % (COL-NUMS "FTR"))) 1 0) (slices RESULTS)))
 (def AWINS (mapv #(if (= "A" (mget % (COL-NUMS "FTR"))) 1 0) (slices RESULTS)))
 
+;; check we have captured all games
+(esum (add HWINS AWINS DRAWS))
+;; => 380
+
 (def HOME-WINS (mapv esum (slices (mul HWINS HOME-GAMES))))
 (def AWAY-WINS (mapv esum (slices (mul AWINS AWAY-GAMES))))
 (def DRAW-RESULTS (mapv esum (slices (add (mul DRAWS AWAY-GAMES) (mul DRAWS HOME-GAMES)))))
